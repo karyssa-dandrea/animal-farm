95bf67d18b17074e9c92f79c0ce546ceeaeda3b7
karyssa-dandrea <karyssadandrea@gmail.com>
2021-09-29 16:36:15 -0700

    added keys to the animal sounds

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // import functions and grab DOM elements
 const catImg = document.getElementById('cat');
 const catSound = document.getElementById('cat-sound');
+const dogImg = document.getElementById('dog');
+const dogSound = document.getElementById('dog-sound');
+const horseImg = document.getElementById('horse');
+const horseSound = document.getElementById('horse-sound');
 // initialize global state
 
 // set event listeners 
@@ -9,7 +13,35 @@ const catSound = document.getElementById('cat-sound');
   // update DOM to reflect the new state
 
 
-  catImg.addEventListener('click', ()=>
-  catSound.play();
-  console.log('clicking the cat!');
-  )
+catImg.addEventListener('click', ()=>{
+catSound.play();
+console.log('clicking the cat!');
+})
+
+dogImg.addEventListener('click', ()=>{
+    dogSound.play();
+    console.log('clicking the dog!');
+    })
+
+horseImg.addEventListener('click', ()=>{
+horseSound.play();
+    console.log('clicking the horse!');
+})
+
+window.addEventListener('keydown', (event) => {
+if (event.key === 'd') {
+catSound.play();
+  }
+})
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'f') {
+  dogSound.play();
+    }
+  })
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'g') {
+    horseSound.play();
+      }
+    })
