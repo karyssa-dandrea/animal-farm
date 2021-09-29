9cfc7cdae74e7fd0be5420b9d5f448defd01abce
karyssa-dandrea <karyssadandrea@gmail.com>
2021-09-29 15:31:44 -0700

    made animal farm with 2 pictures and one audio

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
-
+const catImg = document.getElementById('cat');
+const catSound = document.getElementById('cat-sound');
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  catImg.addEventListener('click', ()=>
+  catSound.play();
+  console.log('clicking the cat!');
+  )
